e6a1dda07d4618ca8e0580e8462b5c1d49a0d259
tony <antik998@mail.ru>
2023-04-13 18:58:18 +0300

    working 6.0.2 without db

---
+++ src/main/java/com/example/micro/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.micro;
+
+import com.example.micro.models.User;
+import com.example.micro.rep.RoleRepository;
+import com.example.micro.rep.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.util.Collection;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        GrantedAuthority authority = new SimpleGrantedAuthority(roleRepository.getByRoleId(user.getRoleId()).getRoleName());
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                (Collection<? extends GrantedAuthority>) authority);
+    }
+}
