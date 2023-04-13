b030e513c5a2a36ba3364b2dbd344d8edc4e546a
tony <antik998@mail.ru>
2023-04-13 13:06:41 +0300

    added security

---
+++ src/main/java/com/example/micro/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.micro;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/","/home").permitAll()
+                        .anyRequest().authenticated()
+                        /*.requestMatchers("/hello").hasRole("ADMIN")*/)
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll())
+                .logout((logout) -> logout.permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> userList = new ArrayList<UserDetails>();
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+        UserDetails admin =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("admin")
+                        .roles("ADMIN")
+                        .build();
+        userList.add(user);
+        userList.add(admin);
+        return new InMemoryUserDetailsManager(userList);
+    }
+
+}
